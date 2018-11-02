5a2d0fa829ed067876d4ac5dc2ffc8b50f697a3c
明 李 <Ltongming@outlook.com>
2018-11-02 15:40:19 +0800

    配置 Api

--- IdentityServer4.Samples/Quickstarts/1_ClientCredentials/src/Api/Startup.cs
+++ IdentityServer4.Samples/Quickstarts/1_ClientCredentials/src/Api/Startup.cs
@@ -15,6 +15,21 @@ namespace Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // 将身份验证服务添加到DI和身份验证中间件到管道
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            // 将身份验证服务添加到DI并配置"Bearer"为默认方案。 
+            services.AddAuthentication("Bearer")
+                // 将IdentityServer访问令牌验证处理程序添加到DI中以供身份验证服务使用。
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ApiName = "api1";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +39,11 @@ namespace Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // 将身份验证中间件添加到管道中，以便在每次调用主机时自动执行身份验证。
+            app.UseAuthentication();
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
---
+++ IdentityServer4.Samples/Quickstarts/1_ClientCredentials/src/Api/readme.md
@@ -0,0 +1,10 @@
+﻿在设置好 IdentityServer4 服务器后
+1. 将IdentityServer4.AccessTokenValidation NuGet包添加到项目中。
+	（这里使用了Microsoft.AspNetCore.Authentication.JwtBearer包来替换 AccessTokenValidation）
+2. 配置 认证 服务。
+	Authenticatiuon: 认证; 身份验证; 证明，鉴定; 密押。
+	Authorization: 授权，批准; 批准（或授权）的证书。
+3. 如果您使用浏览器导航到控制器（http://localhost:5001/identity），您应该获得401状态代码作为回报。
+   这意味着您的API需要凭据。
+
+就是这样，API现在受到IdentityServer的保护。
